8ca371ef632eda4ae75292e07880f7888e065f27
Ivan Sharygin <Charel61@yandex.ru>
2023-03-16 14:56:23 +0300

    changed ggg

--- ggg.py
+++ ggg.py
@@ -1,8 +1,8 @@
-print('hello')
-print (' world')
+from classes___1 import User
+def get_user_info(user: User):
+    return f'Возраст пользователя {user.name} - {user.age}',\
+        f'a e-mail - {user.email}'
 
-def say_saomething(number: int, word: str)-> str:
-    word =word.capitalize()
-    return word*number
 
-print(say_saomething(5,"fuck"))
+user_1: User = User(42, 'Vasiliy',23,'vasya@mail.ru')
+print(get_user_info(user_1))
