ebb31d1a131b16306a5ffdda945500340dbc787e
Charel61 <Charel61@yandex.ru>
2023-03-28 15:21:20 +0300

    chage polling

--- pooling.py
+++ pooling.py
@@ -6,6 +6,7 @@ API_URL: str = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = '6089944231:AAG9SB-CeDIPozAAT7DCNzs7mxplaFOuqLg'
 offset: int = -2
 updates: dict
+timeout: int = 60
 
 def do_somthing() -> None:
     print('Был апдейт')
@@ -13,13 +14,13 @@ def do_somthing() -> None:
 
 while True:
     start_time = time.time()
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             do_somthing()
 
-    time.sleep(3)
+
     end_time = time.time()
     print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
