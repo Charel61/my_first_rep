51b28b2861e87997b83cff36790b039044006104
Charel61 <Charel61@yandex.ru>
2023-03-29 14:52:20 +0300

    change 3 echo-bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,6 +1,6 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message, ContentType
+from aiogram.types import Message
 
 
 API_TOKEN: str ='6089944231:AAG9SB-CeDIPozAAT7DCNzs7mxplaFOuqLg'
@@ -10,8 +10,9 @@ dp: Dispatcher = Dispatcher()
 
 
 @dp.message(Command(commands=["start"]))
-async def process_start_comand(messge: Message):
-    await messge.answer('Привет!\n  Меня зовут Эхо-бот!\nНапиши мне что-нибудь')
+async def process_start_comand(message: Message):
+    print(message.json(indent=4,exclude_none=True))
+    await message.answer('Привет!\n  Меня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
@@ -33,10 +34,13 @@ async def process_help_command(message: Message):
 
 @dp.message()
 async def send_echo(message: Message):
+    print(message.json(indent=4,exclude_none=True))
     try:
         await message.send_copy(chat_id=message.chat.id)
     except:
         await message.reply(text='Данный тип апдейтов не поддерживается методом  send_copy')
+
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
