628192eb9598e794851b8279ab743f6868cb55fb
Charel61 <Charel61@yandex.ru>
2023-03-29 14:33:53 +0300

    change echo-bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -21,7 +21,13 @@ async def send_photo_echo(message: Message):
     print(message)
     await message.reply_photo(message.photo[0].file_id)
 
+async def send_audio_echo(message: Message):
+    print(message)
+    await message.answer_audio(message.audio.file_id)
+
 dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+dp.message.register(send_audio_echo, F.content_type == ContentType.AUDIO)
+
 
 
 
